510768e22b8aa6f19031260608be7131fed742a7
MatanMaor <matanmaor@gmail.com>
2022-08-22 20:13:18 +0300

    upload new files

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const app = express();
+
+dotenv.config({ path: ".env" });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("connected to mongo"))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT;
+const url = process.env.SERVER_URL;
+
+app.listen(port, url, () => {
+  console.log(`listening on ${url}:${port}`);
+});
